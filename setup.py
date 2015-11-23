b1734c7bd5385d25783649b042dae8268009f17f
skyline75489 <skyline75489@outlook.com>
2015-11-23 13:39:04 +0800

    bump version 0.1.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst') as f:
 
 setup(
     name="simpledns",
-    version="0.1.2",
+    version="0.1.4",
     license="MIT",
     description="A lightweight yet useful proxy DNS server",
     author='skyline75489',
--- simpledns/dnsproxy.py
+++ simpledns/dnsproxy.py
@@ -20,7 +20,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-__version__ = '0.1.2'
+__version__ = '0.1.4'
 
 import os
 import sys
