06fd6bdf3738ab18af81e74774bc3506d199197b
skyline75489 <skyline75489@outlook.com>
2016-10-09 20:38:33 +0800

    2016

--- simpledns/dnsproxy.py
+++ simpledns/dnsproxy.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-# Copyright (c) 2014 skyline75489
+# Copyright (c) 2016 skyline75489
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
